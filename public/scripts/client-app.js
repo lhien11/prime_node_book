61448218f171a018cfff4b067b0ea48529ae7682
Thao Nguyen Le <lhien11@Thaos-MacBook-Air.local>
2016-11-16 09:32:01 -0600

    It's working

---
+++ public/scripts/client-app.js
@@ -0,0 +1,64 @@
+$(document).ready(function () {
+    getBooks();
+
+    // add a book
+    $('#book-submit').on('click', postBook);
+});
+/**
+ * Retrieve books from server and append to DOM
+ */
+function getBooks() {
+  $.ajax({
+    type: 'GET',
+    url: '/books',
+    success: function(books) {
+      appendBooks(books);
+    },
+    error: function() {
+      console.log('Database error');
+    }
+
+  })
+}
+/**
+ * Add a new book to the database and refresh the DOM
+ */
+function postBook() {
+  event.preventDefault();
+
+  var book = {};
+
+  $.each($('#book-form').serializeArray(), function (i, field) {
+        book[field.name] = field.value;
+      });
+
+  console.log('book: ', book);
+
+  $.ajax({
+    type: 'POST',
+    url: '/books',
+    data: book,
+    success: function(response) {
+      getBooks();
+    },
+    error: function() {
+      console.log('could not post a new book');
+    }
+  })
+
+}
+
+function appendBooks(books) {
+  $("#book-list").empty();
+
+  for (var i = 0; i < books.length; i++) {
+    $("#book-list").append('<div class="row book"></div>');
+    $el = $('#book-list').children().last();
+    $el.append('<h2> ' + books[i].title + '</h2>');
+    $el.append('<p> ' + books[i].author + '</p>');
+    $el.append('<p> ' + books[i].genre + '</p>');
+    $el.append('<p> ' + books[i].published + '</p>');
+    $el.append('<p> ' + books[i].edition + '</p>');
+    $el.append('<p> ' + books[i].publisher + '</p>');
+  }
+}
---
+++ routes/books.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var router = express.Router();
+var pg = require('pg');
+var connectionString = 'postgres://localhost:5432/sigma';
+
+router.get('/', function(req, res) {
+  console.log('get request');
+  // get books from DB
+  pg.connect(connectionString, function(err, client, done) {
+    if(err) {
+      console.log('connection error: ', err);
+      res.sendStatus(500);
+    }
+
+    client.query('SELECT * FROM books', function(err, result) {
+      done(); // close the connection.
+
+      // console.log('the client!:', client);
+
+      if(err) {
+        console.log('select query error: ', err);
+        res.sendStatus(500);
+      }
+      res.send(result.rows);
+
+    });
+
+  });
+});
+
+router.post('/', function(req, res) {
+  var newBook = req.body;
+  pg.connect(connectionString, function(err, client, done) {
+    if(err) {
+      console.log('connection error: ', err);
+      res.sendStatus(500);
+    }
+
+    client.query(
+      'INSERT INTO books (title, author, published, genre, edition, publisher) ' +
+      'VALUES ($1, $2, $3, $4, $5, $6)',
+      [newBook.title, newBook.author, newBook.published, newBook.genre, newBook.edition, newBook.publisher],
+      function(err, result) {
+        done();
+
+        if(err) {
+          console.log('insert query error: ', err);
+          res.sendStatus(500);
+        } else {
+          res.sendStatus(200);
+        }
+      });
+
+  });
+
+});
+
+
+module.exports = router;
